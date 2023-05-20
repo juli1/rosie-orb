f42d866ccf41d6e3c8d35ae7c9b917c064bbdad8
Julien Delange <julien.delange@datadoghq.com>
2023-05-20 19:05:24 -0400

    update

--- src/scripts/analyze.sh
+++ src/scripts/analyze.sh
@@ -40,7 +40,6 @@ export DD_APP_KEY=${PARAM_APP_KEY}
 export DD_ENV=${PARAM_DD_ENV}
 export DD_SERVICE=${PARAM_DD_SERVICE}
 export DD_SITE=${PARAM_DD_SITE}
-eval echo \$$PARAM_DD_SITE
 echo "DD_API_KEY: ${DD_API_KEY}"
 echo "DD_APP_KEY: $DD_APP_KEY"
 echo "DD_ENV:     $DD_ENV"
