e6157bc99232941abc56d48d72bc75454c1433da
Julien Delange <julien.delange@datadoghq.com>
2023-05-20 18:32:00 -0400

    update

--- src/scripts/analyze.sh
+++ src/scripts/analyze.sh
@@ -45,12 +45,11 @@ echo "$DD_SITE"
 ########################################################
 # install Java
 ########################################################
-
+echo -n "Install dependencies ... "
 sudo apt-get update >/dev/null 2>&1
 sudo apt-get install --yes openjdk-17-jdk >/dev/null 2>&1
 sudo apt-get install --yes unzip >/dev/null 2>&1
-
-
+echo "done"
 
 ########################################################
 # check variables
@@ -80,6 +79,7 @@ PROJECT_ROOT=$(pwd)
 ########################################################
 # static analyzer tool stuff
 ########################################################
+echo -n "Install datadog static analyzer ... "
 TOOL_DIRECTORY=$(mktemp -d)
 
 if [ ! -d "$TOOL_DIRECTORY" ]; then
@@ -91,12 +91,12 @@ cd "$TOOL_DIRECTORY" || exit 1
 curl -L -O http://dtdg.co/latest-static-analyzer >/dev/null 2>&1 || exit 1
 unzip latest-static-analyzer > /dev/null 2>&1 || exit 1
 CLI_LOCATION=$TOOL_DIRECTORY/cli-1.0-SNAPSHOT/bin/cli
-
+echo "done"
 
 ########################################################
 # datadog-ci stuff
 ########################################################
-echo "Installing 'datadog-ci'"
+echo -n "Install datadog-ci ..."
 sudo /usr/local/bin/npm install -g @datadog/datadog-ci || exit 1
 
 DATADOG_CLI_PATH=/usr/local/bin/datadog-ci
@@ -106,13 +106,12 @@ if [ ! -x $DATADOG_CLI_PATH ]; then
     echo "The datadog-ci was not installed correctly, not found in $DATADOG_CLI_PATH."
     exit 1
 fi
-
-echo "Done: datadog-ci available $DATADOG_CLI_PATH"
+echo "done: datadog-ci available $DATADOG_CLI_PATH"
 
 ########################################################
 # output directory
 ########################################################
-echo "Getting output directory"
+echo -n "Getting output directory ... "
 OUTPUT_DIRECTORY=$(mktemp -d)
 
 # Check that datadog-ci was installed
@@ -123,17 +122,17 @@ fi
 
 OUTPUT_FILE="$OUTPUT_DIRECTORY/output.sarif"
 
-echo "Done: will output results at $OUTPUT_FILE"
+echo "done: will output results at $OUTPUT_FILE"
 
 ########################################################
 # execute the tool and upload results
 ########################################################
 
-echo "Starting a static analysis"
+echo -n "Starting a static analysis ..."
 $CLI_LOCATION --directory "${PROJECT_ROOT}" -t true -o "$OUTPUT_FILE" -f sarif || exit 1
-echo "Done"
+echo "done"
 
 
-echo "Uploading results to Datadog"
+echo -n "Uploading results to Datadog ..."
 ${DATADOG_CLI_PATH} sarif upload "$OUTPUT_FILE" --service "$DD_SERVICE" --env "$DD_ENV"
 echo "Done"
